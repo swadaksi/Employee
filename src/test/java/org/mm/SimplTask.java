429ade10dc1a2644906ad3efdeaa42ae1be4e99d
swadaksi <sswathi1917@gmail.com>
2024-09-13 15:34:24 +0530

    message

---
+++ src/test/java/org/mm/SimplTask.java
@@ -0,0 +1,16 @@
+package org.mm;
+
+public class SimplTask {
+private void m1() {
+	// TODO Auto-generated method stub
+
+}
+private void m2() {
+	// TODO Auto-generated method stub
+
+}
+private void m3() {
+	// TODO Auto-generated method stub
+
+}
+}
